198005c303d193f59a938b4a19ebac72d1fc41f9
saifulriza123 <mail.saifulriza@gmail.com>
2020-09-09 17:47:44 +0700

    Fix alphabet

--- lib/models/menu.dart
+++ lib/models/menu.dart
@@ -11,6 +11,6 @@ class Menu {
 const List<Menu> allMenus = <Menu>[
   Menu('Home', Icons.home, Colors.white, Color(0xFF4d79fe)),
   Menu('Statistics', Icons.insert_chart, Colors.grey, null),
-  Menu('note', Icons.event_note, Colors.grey, null),
+  Menu('Note', Icons.event_note, Colors.grey, null),
   Menu('Info', Icons.info, Colors.grey, null)
 ];
